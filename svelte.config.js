85202fd03479dfb99e8e4bddc5b784e96c680133
irinainina <irbris@mail.ru>
2023-05-30 15:28:20 +0300

    feat: add adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
 // import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-node";
 import typescript from "@rollup/plugin-typescript";
 
 const config = {
