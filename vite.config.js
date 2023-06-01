6a040303c9f978040fae0a6a02fc449379439211
irinainina <irbris@mail.ru>
2023-06-01 16:31:51 +0300

    fix: update environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    EnvironmentPlugin(["VITE_NOTION_API_KEY", "VITE_NOTION_DATABASE_ID"]),
+    EnvironmentPlugin({
+      VITE_NOTION_API_KEY: "secret_Cxqe9YB8Hr5yQDr1irvyyXsLABmnDQtCuaqNyDYiAZs",
+      VITE_NOTION_DATABASE_ID: "d0cceabd34b54d03a2d6c3bedb2df826",
+    }),
   ],
   server: {
     port: 3000,
